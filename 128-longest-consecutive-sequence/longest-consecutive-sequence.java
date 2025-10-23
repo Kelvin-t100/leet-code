75504d34067f3ca573bd1db5be625eced612639c
kelvin <kelvinkuruvila@gmail.com>
2025-10-23 15:13:16 +0530

    Time: 18 ms (88.51%) | Memory: 57 MB (82.05%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -2,7 +2,6 @@ class Solution {
     public int longestConsecutive(int[] arr) {
         if(arr.length==0) return 0;
         Arrays.sort(arr);int t=0;
-        
         int c=0,high=0;
     for(int i=0;i<arr.length-1;i++){
           if(arr[i]==arr[i+1])continue;
