d3f1dedeaf32b35d30e0dd203542585167491615
kelvin <kelvinkuruvila@gmail.com>
2026-07-16 00:11:29 +0530

    Time: 139 ms (5.09%) | Memory: 48 MB (20.33%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int arr[]=new int[nums.length];
+        for(int i=0,j=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                arr[j]=nums[i];j++;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            System.out.println(arr[i]);
+            nums[i]=arr[i];
+        }
+    
+    }
+}
